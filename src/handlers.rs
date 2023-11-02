fdaaa80baa0842ec000fddfac25ca967cdd0620c
sugimoriyoshiki <sugimoriyoshiki@team-lab.com>
2023-11-02 21:35:19 +0900

    fix: todoの実装

---
+++ src/handlers.rs
@@ -0,0 +1,55 @@
+use axum::{
+    extract::{Extension, Path},
+    http::StatusCode,
+    response::IntoResponse,
+    Json,
+};
+use std::sync::Arc;
+
+use crate::repositories::{CreateTodo, TodoRepository, UpdateTodo};
+
+// https://qiita.com/Sicut_study/items/5e5d6cce5ba48c225367
+pub async fn create_todo<T: TodoRepository>(
+    Extension(repository): Extension<Arc<T>>,
+    Json(payload): Json<CreateTodo>,
+) -> impl IntoResponse {
+    let todo = repository.create(payload);
+
+    (StatusCode::CREATED, Json(todo))
+}
+
+pub async fn find_todo<T: TodoRepository>(
+    Path(id): Path<i32>,
+    Extension(repository): Extension<Arc<T>>,
+) -> Result<impl IntoResponse, StatusCode> {
+    let todo = repository.find(id).ok_or(StatusCode::NOT_FOUND)?;
+    Ok((StatusCode::OK, Json(todo)))
+}
+
+pub async fn all_todo<T: TodoRepository>(
+    Extension(repository): Extension<Arc<T>>,
+) -> impl IntoResponse {
+    let todo = repository.all();
+    (StatusCode::OK, Json(todo))
+}
+
+pub async fn update_todo<T: TodoRepository>(
+    Extension(repository): Extension<Arc<T>>,
+    Path(id): Path<i32>,
+    Json(payload): Json<UpdateTodo>,
+) -> Result<impl IntoResponse, StatusCode> {
+    let todo = repository
+        .update(id, payload)
+        .or(Err(StatusCode::NOT_FOUND))?;
+    Ok((StatusCode::CREATED, Json(todo)))
+}
+
+pub async fn delete_todo<T: TodoRepository>(
+    Path(id): Path<i32>,
+    Extension(repository): Extension<Arc<T>>,
+) -> StatusCode {
+    repository
+        .delete(id)
+        .map(|_| StatusCode::NO_CONTENT)
+        .unwrap_or(StatusCode::NOT_FOUND)
+}
